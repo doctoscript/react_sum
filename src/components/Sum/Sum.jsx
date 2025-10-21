6807b728a6c8c68cb193efebfaeacd1317103857
Pavlo Mikitjuk <mikitjukpavlo@gmail.com>
2025-10-21 22:18:31 +0300

    add best

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,9 +1,7 @@
-export const Sum = ({ a = 0, b = 0 }) => {
+export const Sum = ({ a: propA, b: propB }) => {
+  const a = Number(propA) || 0;
+  const b = Number(propB) || 0;
   const result = a + b;
 
-  return (
-    <p>
-      Sum of {a} and {b} is {result}
-    </p>
-  );
+  return <p>{`Sum of ${a} and ${b} is ${result}`}</p>;
 };
